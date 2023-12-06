bfb21ba20b6488b0ed87c56e3d411bf1c14e5c7a
Robert Gleason <l2ob122@gmail.com>
2023-12-05 20:49:42 -0800

    added so the pipes will leave the tree when they are passed

--- plane_cb/plane_cb.gd
+++ plane_cb/plane_cb.gd
@@ -6,29 +6,29 @@ const POWER: float = -400.0
 @onready var animation_player = $AnimationPlayer
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass  # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	velocity.y += GRAVITY * delta # How many pixesls its falling per second
-	
+	velocity.y += GRAVITY * delta  # How many pixesls its falling per second
+
 	fly()
 	move_and_slide()
-	
+
 	if is_on_floor() == true:
 		die()
 
-func fly() ->void:
+
+func fly() -> void:
 	if Input.is_action_just_pressed("fly") == true:
 		velocity.y = POWER
 		animation_player.play("fly")
 
+
 func die() -> void:
 	animated_sprite_2d.stop()
 	set_physics_process(false)
-	
-	
-	
