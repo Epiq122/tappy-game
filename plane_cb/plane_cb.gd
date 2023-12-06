a2e9c5cec0e5b2634705ed2e8d0ed8cfa9cedb2b
Robert Gleason <l2ob122@gmail.com>
2023-12-05 18:54:06 -0800

    added a invisble floor so when the plane hits the bottom it stops

--- plane_cb/plane_cb.gd
+++ plane_cb/plane_cb.gd
@@ -4,6 +4,7 @@ const GRAVITY: float = 1900.0
 const POWER: float = -400.0
 
 @onready var animation_player = $AnimationPlayer
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,12 +16,19 @@ func _physics_process(delta):
 	velocity.y += GRAVITY * delta # How many pixesls its falling per second
 	
 	fly()
-	
-	
-	
 	move_and_slide()
+	
+	if is_on_floor() == true:
+		die()
 
 func fly() ->void:
 	if Input.is_action_just_pressed("fly") == true:
 		velocity.y = POWER
 		animation_player.play("fly")
+
+func die() -> void:
+	animated_sprite_2d.stop()
+	set_physics_process(false)
+	
+	
+	
